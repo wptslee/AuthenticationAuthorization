cc26df33247672ca73998c00a7e7942b9726da3b
tslee <tslee3375@naver.com>
2021-03-08 22:38:13 +0900

    Append. Login/{Username}, InfoDetails

--- AuthenticationAuthorization/AuthenticationAuthorization/Startup.cs
+++ AuthenticationAuthorization/AuthenticationAuthorization/Startup.cs
@@ -38,19 +38,25 @@ namespace AuthenticationAuthorization
 
             app.UseEndpoints(endpoints =>
             {
+                #region Menu
                 endpoints.MapGet("/", async context =>
                 {
                     string content = "<h1>ASP.NET Core 인증과 권한 초간단 코드</h1>";
 
                     content += "<a href=\"/Login\">로그인</a><br />";
+                    content += "<a href=\"/Login/User\">로그인(User)</a><br />";
+                    content += "<a href=\"/Login/Administrator\">로그인(Administrator)</a><br />";
                     content += "<a href=\"/Info\">정보</a><br />";
+                    content += "<a href=\"/InfoDetails\">정보(Details)</a><br />";
                     content += "<a href=\"/InfoJson\">정보(JSON)</a><br />";
                     content += "<a href=\"/Logout\">로그아웃</a><br />";
 
                     context.Response.Headers["Content-Type"] = "text/html; charset=utf-8"; // ko-KR
                     await context.Response.WriteAsync(content);
                 });
+                #endregion
 
+                #region Login
                 endpoints.MapGet("/Login", async context =>
                 {
                     var claims = new List<Claim>
@@ -68,6 +74,41 @@ namespace AuthenticationAuthorization
                     context.Response.Headers["Content-Type"] = "text/html; charset=utf-8"; // ko-KR
                     await context.Response.WriteAsync(content);
                 });
+                #endregion
+
+                #region Login/{Username}
+                endpoints.MapGet("/Login/{Username}", async context =>
+                {
+                    var username = context.Request.RouteValues["Username"].ToString();
+
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, username),
+                        new Claim(ClaimTypes.Name, username),
+                        new Claim(ClaimTypes.Email, username + @"@a.com"),
+                        new Claim(ClaimTypes.Role, "Users"),
+                        new Claim("원하는 이름", "원하는 값"),
+                    };
+
+                    if (username == "Administrator")
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, "Administrators"));
+                    }
+
+                    var claimIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    var claimPrincipal = new ClaimsPrincipal(claimIdentity);
+
+                    await context.SignInAsync(
+                        CookieAuthenticationDefaults.AuthenticationScheme,
+                        claimPrincipal,
+                        new AuthenticationProperties { IsPersistent = true }); // 쿠키 영구 저장
+
+                    string content = "<h3>로그인 완료</h3>";
+                    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8"; // ko-KR
+                    await context.Response.WriteAsync(content);
+                });
+                #endregion
 
                 #region Info
                 endpoints.MapGet("/Info", async context =>
@@ -77,6 +118,35 @@ namespace AuthenticationAuthorization
                     if (context.User.Identity.IsAuthenticated)
                     {
                         result += $"<h3>로그인 이름 : {context.User.Identity.Name}</h3>";
+                    }
+                    else
+                    {
+                        result += "<h3>로그인하지 않았습니다.</h3>";
+                    }
+
+                    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8"; // ko-KR
+                                                                                           //await context.Response.WriteAsync(result, Encoding.Default);
+                    await context.Response.WriteAsync(result);
+                });
+                #endregion
+
+                #region InfoDetails
+                endpoints.MapGet("/InfoDetails", async context =>
+                {
+                    string result = "";
+
+                    if (context.User.Identity.IsAuthenticated)
+                    {
+                        result += $"<h3>로그인 이름 : {context.User.Identity.Name}</h3>";
+                        foreach (var claim in context.User.Claims)
+                        {
+                            result += $"{claim.Type} = {claim.Value}<br />";
+                        }
+
+                        if (context.User.IsInRole("Administrators") && context.User.IsInRole("Users"))
+                        {
+                            result += $"<br />Administrators + Users 권한이 있습니다.<br />";
+                        }
                     }
                     else
                     {
@@ -118,10 +188,11 @@ namespace AuthenticationAuthorization
                 endpoints.MapGet("/Logout", async context =>
                 {
                     await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
                     string content = "<h3>로그아웃 완료</h3>";
                     context.Response.Headers["Content-Type"] = "text/html; charset=utf-8"; // ko-KR
                     await context.Response.WriteAsync(content);
-                }); 
+                });
                 #endregion
             });
         }
